df49095e528edfb4e6a3d09f5341f112b88b36b8
Lucas Salvador Amorim <lucassalvadoramorim@gmail.com>
2023-04-25 19:43:41 +0000

    Parametros

--- app/src/main/java/tarefaspring/controller/IndexController.java
+++ app/src/main/java/tarefaspring/controller/IndexController.java
@@ -1,13 +1,16 @@
 package tarefaspring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 
 public class IndexController {
     @RequestMapping("/")
-    public String home(){
+    public String home(@RequestParam("nome") String nome, Model model){
+        model.addAttribute("mensagem", "Olá" + nome);
         return "home.jsp";
     }
 }
