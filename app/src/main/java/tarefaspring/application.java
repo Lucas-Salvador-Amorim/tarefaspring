8ba26bdec694a7ef60de4fa5d3d3e44e5a52bb87
Lucas Salvador Amorim <lucassalvadoramorim@gmail.com>
2023-04-25 18:41:01 +0000

    app

---
+++ app/src/main/java/tarefaspring/application.java
@@ -0,0 +1,12 @@
+package tarefaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class application{
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
